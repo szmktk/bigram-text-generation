09362e9ed1c444882c9aca73ea18f6569f911b14
Szymon Mikitiuk <recipes.jackpot.0c@icloud.com>
2025-06-11 13:29:12 +0200

    Add a method to randomly choose one of the most common bigrams to start with

--- main.py
+++ main.py
@@ -32,7 +32,11 @@ def main() -> None:
     if args.top_bigrams > 0:
         print_most_common_bigrams(bigram_counts, args.top_bigrams)
 
-    initial_bigram = tuple(args.initial_bigram.split(","))
+    initial_bigram = (
+        tuple(args.initial_bigram.split(","))
+        if args.initial_bigram
+        else choose_random_bigram(bigram_counts, args.print_bigram)
+    )
     word1, word2 = initial_bigram
 
     for _ in range(15):
@@ -51,6 +55,17 @@ def print_most_common_bigrams(bigram_counts: Counter, n_common_bigrams: int) -> 
     print()
 
 
+def choose_random_bigram(bigram_counts: Counter, print_bigram: bool) -> tuple[str, str]:
+    top_bigrams = bigram_counts.most_common(50)
+    if not top_bigrams:
+        raise ValueError("Not enough bigrams available to choose from.")
+
+    chosen_bigram = random.choice([bigram for bigram, _ in top_bigrams])
+    if print_bigram:
+        print(f"Randomly chosen bigram: {chosen_bigram}")
+    return chosen_bigram
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Generate text from a text file using bigrams."
@@ -59,8 +74,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--initial-bigram",
         "-i",
-        help="Comma-separated words to start generation. They need to form a bigram that is present in the source text. Default is 'the,lawyer'.",
-        default="the,lawyer",
+        help="Comma-separated words to start generation. They need to form a bigram that is present in the source text.",
     )
     parser.add_argument(
         "--no-clean",
@@ -74,7 +88,13 @@ if __name__ == "__main__":
         "-t",
         type=int,
         default=0,
-        help="Print top n most common bigrams found in the source text.",
+        help="Print top N most common bigrams found in the source text.",
+    )
+    parser.add_argument(
+        "--print-bigram",
+        "-p",
+        action="store_true",
+        help="Print the randomly chosen bigram among the top N bigrams.",
     )
     args = parser.parse_args()
     main()
--- todo.md
+++ todo.md
@@ -6,8 +6,8 @@
     - actually one parameter of type tuple (must be two words, separated by comma)
 - [x] add option to ignore word cleaning
 - [x] add a method to print or examine the bigrams (most common ones? for easy bootstrapping)
-- [ ] add a method to randomly choose one of the most common bigrams to start with
-    - [ ] print the starting bigram
+- [x] add a method to randomly choose one of the most common bigrams to start with
+    - [x] print the starting bigram
 - [ ] add a capability to read all `.txt` files from `data/` directory
     - [ ] make the CLI flag to read input file optional (should point to entire directory)
 - [ ]
