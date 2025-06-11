cebf0af8aaf8ba8053434dc3551963430233bc1b
Szymon Mikitiuk <recipes.jackpot.0c@icloud.com>
2025-06-11 14:09:44 +0200

    Read all .txt files from data directory

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 import argparse
+import glob
+import os
 import random
 from collections import Counter
+from io import TextIOWrapper
 
 # set the seed of PRNG to get predictable results
 # random.seed(3)
 
 
 def main() -> None:
-    reader = open(args.input_file_path)
-    bigram_counts = Counter()
-    successor_map = {}
-    window = []
+    # bigram_counts = Counter()
+    # successor_map = {}
 
-    for line in reader:
-        for word in line.split():
-            clean_word = (
-                word.strip(".;,-“’”:?—‘!()_").lower() if args.clean_words else word
-            )
-            window.append(clean_word)
-
-            if len(window) == 3:
-                key = window[0], window[1]
-                value = window[2]
-                if key in successor_map:
-                    successor_map[key].append(value)
-                else:
-                    successor_map[key] = [value]
-                bigram_counts[key] += 1
-                window.pop(0)
+    successor_map, bigram_counts = process_files(args.input_dir, args.clean_words)
 
     if args.top_bigrams > 0:
         print_most_common_bigrams(bigram_counts, args.top_bigrams)
@@ -41,7 +27,9 @@ def main() -> None:
 
     for _ in range(15):
         print(word1, end=" ")
-        successors = successor_map[(word1, word2)]
+        successors = successor_map.get((word1, word2), None)
+        if not successors:
+            break
         word3 = random.choice(successors)
         word1, word2 = word2, word3
 
@@ -66,11 +54,54 @@ def choose_random_bigram(bigram_counts: Counter, print_bigram: bool) -> tuple[st
     return chosen_bigram
 
 
+def process_files(input_dir: str, clean_words: bool) -> tuple[dict, Counter]:
+    successor_map = {}
+    bigram_counts = Counter()
+    for file_path in glob.glob(os.path.join(input_dir, "*.txt")):
+        with open(file_path, "r") as reader:
+            process_reader(reader, successor_map, bigram_counts, clean_words)
+    return successor_map, bigram_counts
+
+
+def process_reader(
+    reader: TextIOWrapper,
+    successor_map: dict,
+    bigram_counts: Counter,
+    clean_words: bool,
+) -> None:
+    # TODO can we move the window deeper?
+    window = []
+    for line in reader:
+        for word in line.split():
+            clean_word = word.strip(".;,-“’”:?—‘!()_").lower() if clean_words else word
+            update_window_and_maps(window, clean_word, successor_map, bigram_counts)
+
+
+def update_window_and_maps(
+    window: list, clean_word: str, successor_map: dict, bigram_counts: Counter
+):
+    window.append(clean_word)
+    if len(window) == 3:
+        key = window[0], window[1]
+        value = window[2]
+        if key in successor_map:
+            successor_map[key].append(value)
+        else:
+            successor_map[key] = [value]
+        bigram_counts[key] += 1
+        window.pop(0)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Generate text from a text file using bigrams."
+        description="Generate text from .txt files using bigrams."
     )
-    parser.add_argument("input_file_path", help="Path to the input text file.")
+    parser.add_argument(
+        "--input-dir",
+        "-d",
+        help="Path to the directory with text files. Default is 'data'.",
+        default="data",
+    )
     parser.add_argument(
         "--initial-bigram",
         "-i",
--- todo.md
+++ todo.md
@@ -9,5 +9,7 @@
 - [x] add a method to randomly choose one of the most common bigrams to start with
     - [x] print the starting bigram
 - [ ] add a capability to read all `.txt` files from `data/` directory
-    - [ ] make the CLI flag to read input file optional (should point to entire directory)
-- [ ]
+    - [x] make the CLI flag to read input file optional (should point to entire directory)
+    - [ ] reduce the number of function params by adding more globals
+- [ ] remove magic numbers (15 & 50)
+- [ ] move the project to `worktrees` dir & add `screenshots/` directory
